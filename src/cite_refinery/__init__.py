67bfa73992e585d94687bd1b4ab831c751affdfa
spectating101 <s1133958@mail.yzu.edu.tw>
2026-09-08 00:14:23 +0800

    Export Problem Commons kernel from package

--- src/cite_refinery/__init__.py
+++ src/cite_refinery/__init__.py
@@ -1,6 +1,7 @@
-"""Cite-Refinery: evidence-to-solution project orchestration."""
+"""Cite-Refinery and the experimental Problem Commons coordination kernel."""
 
 from .orchestrator import CiteRefinery
+from .problem_commons import ProblemCommons, ProblemPacket, ProblemStatus
 
-__all__ = ["CiteRefinery"]
+__all__ = ["CiteRefinery", "ProblemCommons", "ProblemPacket", "ProblemStatus"]
 __version__ = "0.2.0"
